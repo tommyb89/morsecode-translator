384fbe4806abb484cf0ca743074e5ea3bee6fba3
Tomasz <mandalorian99@gmail.com>
2021-11-22 12:22:41 +0000

    added buttons and writing a first function

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 import { morseCode } from "./morse.js";
+
+// Select inputs
+const morseInput = document.getElementById("#morse");
+const englishInput = document.getElementById("#morse");
+const output = document.querySelector(".output");
+// Function to decode symbols
+const decodeSymbol = (symbol) => {};
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,3 +1,10 @@
 "use strict";
 
 var _morse = require("./morse.js");
+
+// Select inputs
+var morseInput = document.getElementById("#morse");
+var englishInput = document.getElementById("#morse");
+var output = document.querySelector(".output"); // Function to decode symbols
+
+var decodeSymbol = function decodeSymbol(symbol) {};
