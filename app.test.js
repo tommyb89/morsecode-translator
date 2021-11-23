cc987e09eb8575555fc89a6601c1578442863469
Tomasz B <69089650+tommyb89@users.noreply.github.com>
2021-11-23 11:51:12 +0000

    Delete app.test.js

--- app.test.js
+++
@@ -1,9 +0,0 @@
-import { expect, it } from "@jest/globals";
-import { encodeToMorse } from "./app";
-
-// Translate symbols to letters
-
-it("should translate A ", () => {
-  const result = encodeToMorse("A");
-  expect(result).toBe(".-");
-});
